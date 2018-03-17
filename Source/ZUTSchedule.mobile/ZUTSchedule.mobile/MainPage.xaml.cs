3417229ad22cd14657d5f7c9f019fc2d5254b47c
Krzysztof Kołodziejczak <mrthemo@gmail.com>
2018-03-17 19:27:46 +0100

    Add Xamarin Standard project

---
+++ Source/ZUTSchedule.mobile/ZUTSchedule.mobile/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Simple.Xamarin.Framework.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ZUTSchedule.mobile
+{
+	public partial class MainPage : BaseContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BindingContext = new BasePageViewModel()
+            {
+                PageTitle = "Simple Test",
+                LeftButtonTitle = "Left",
+                RightButtonTitle = "Right",
+            };
+        }
+	}
+}
